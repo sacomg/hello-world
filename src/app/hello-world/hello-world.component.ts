6bef27c6f00df238764d510ece62b4a97ab97abd
sacomg <sacomg@gmail.com>
2017-05-13 17:05:03 +0200

    subida

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.css']
 })
-export class HelloWorldComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HelloWorldComponent {
 
 }
